bd5919c4cac0ba421387f895280d8ff2c8924419
Brendon Crowe <brendon.m.crowe@gmail.com>
2022-12-08 22:33:49 -0500

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Brendon Crowe on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
